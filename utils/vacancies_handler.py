38cce0e149a172f662629f5982d301e4b6dd6766
Anna Raketa <rocketvsaliens@gmail.com>
2023-09-20 00:16:07 +0300

    Delete ghosts (moved and removed files)

--- utils/vacancies_handler.py
+++
@@ -1,6 +0,0 @@
-def filter_by_salary(vacancies: list) -> list:
-    """
-    Возвращает только вакансии с зарплатой
-    """
-    return [vacancy for vacancy in vacancies
-            if vacancy.get_avg_salary() != 0]
